720e16891078c6faac70d8cf3bcd9351c55459db
Jorge Vargas <joluvargaspa@ittepic.edu.mx>
2026-05-28 15:09:45 -0700

    env

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const enviroment = {
-    apiURL: 'http://localhost:3000',
-    PREN_ITT_URL: 'http://localhost:4200',
+    //apiURL: 'http://localhost:3000',
+    //PREN_ITT_URL: 'http://localhost:4200',
 
-    //apiURL: 'https://prenacional-2026-itt-api.onrender.com',
-    //PREN_ITT_URL: 'https://prenacional-2026-itt.vercel.app',
+    apiURL: 'https://prenacional-2026-itt-api.onrender.com',
+    PREN_ITT_URL: 'https://prenacional-2026-itt.vercel.app',
 }
